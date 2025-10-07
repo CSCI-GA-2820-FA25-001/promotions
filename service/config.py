f99d9ac10e378dcc074c441325bcaca494723a05
Jackie Wen <129074656+ReidenGS@users.noreply.github.com>
2025-10-06 20:14:20 -0400

    Update config.py

--- service/config.py
+++ service/config.py
@@ -6,8 +6,7 @@ import logging
 
 # Get configuration from environment
 DATABASE_URI = os.getenv(
-    "DATABASE_URI",
-    "postgresql+psycopg://postgres:postgres@localhost:5432/postgres"
+    "DATABASE_URI"
 )
 
 # Configure SQLAlchemy
@@ -18,3 +17,4 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 # Secret for session management
 SECRET_KEY = os.getenv("SECRET_KEY", "sup3r-s3cr3t")
 LOGGING_LEVEL = logging.INFO
+
