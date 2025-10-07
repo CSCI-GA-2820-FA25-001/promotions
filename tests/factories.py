d4ccd03080f7159c1c19d7547c31a82f55c22acc
Jackie Wen <129074656+ReidenGS@users.noreply.github.com>
2025-10-07 15:52:04 -0400

    Update factories.py

--- tests/factories.py
+++ tests/factories.py
@@ -3,18 +3,25 @@ Test Factory to make fake objects for testing
 """
 
 import factory
-from service.models import YourResourceModel
+from datetime import datetime, timedelta
+from service.models import Promotion, DiscountTypeEnum, PromotionTypeEnum, StatusEnum,db
 
 
-class YourResourceModelFactory(factory.Factory):
+class PromotionFactory(factory.Factory):
     """Creates fake pets that you don't have to feed"""
 
-    class Meta:  # pylint: disable=too-few-public-methods
-        """Maps factory to data model"""
+    class Meta:
+        model = Promotion
 
-        model = YourResourceModel
-
-    id = factory.Sequence(lambda n: n)
-    name = factory.Faker("first_name")
+    id = factory.Sequence(lambda n: n + 1)
+    product_name = factory.Sequence(lambda n: f"Product_{n}")
+    description = factory.Faker("sentence", nb_words=6)
+    original_price = factory.Faker("pydecimal", left_digits=3, right_digits=2, positive=True)
+    discount_value = factory.Faker("pydecimal", left_digits=2, right_digits=2, positive=True)
+    discount_type = DiscountTypeEnum.amount
+    promotion_type = PromotionTypeEnum.discount
+    start_date = factory.LazyFunction(datetime.now)
+    expiration_date = factory.LazyFunction(lambda: datetime.now() + timedelta(days=30))
+    status = StatusEnum.draft
 
     # Todo: Add your other attributes here...
