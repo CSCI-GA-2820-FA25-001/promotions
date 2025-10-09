5cefe5da0f5943b4b6f8abd4356fd27ad0671250
MilkyCode <145179065+light12222@users.noreply.github.com>
2025-10-09 17:04:10 -0400

    Add tests for Issue #8 (List Promotions) to improve coverage
    [Tests] Add role-based List Promotions test cases (#8)
    Includes customer/supplier/manager scenarios and invalid role handling

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,7 +25,8 @@ from unittest import TestCase
 from wsgi import app
 from service.common import status
 # from service.models import db, YourResourceModel
-from service.models import db, Promotion
+from service.models import db, Promotion, StatusEnum
+from .factories import PromotionFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
@@ -68,10 +69,68 @@ class TestYourResourceService(TestCase):
     ######################################################################
     #  P L A C E   T E S T   C A S E S   H E R E
     ######################################################################
-
+    # Todo: Add your test cases here...
     def test_index(self):
-        """It should call the home page"""
+        """It should call the home page and return service info"""
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        body = resp.get_json()
+        self.assertIn("name", body)
+        self.assertIn("version", body)
+        self.assertIn("endpoints", body)
+        self.assertIn("list_promotions", body["endpoints"]) 
+    ######################################################################
+    # 8 #  T E S T   L I S T   P R O M O T I O N S
+    ######################################################################
+    def test_list_promotions_empty(self):
+        """It should return [] when there is no data"""
+        resp = self.client.get("/promotions")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertIsInstance(data, list)
+        self.assertEqual(len(data), 0)
+    # customer
+    def test_list_promotions_customer_only_active(self):
+        """It should list ONLY active promotions for customers"""
+        active = PromotionFactory(status=StatusEnum.active)
+        expired = PromotionFactory(status=StatusEnum.expired)
+        active.create()
+        expired.create()
+    
+        resp = self.client.get("/promotions?role=customer")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 1)
+        self.assertEqual(data[0]["status"], "active")
+    # supplier
+    def test_list_promotions_supplier_active_and_expired(self):
+        """Supplier should see active and expired, but NOT deleted"""
+        a = PromotionFactory(status=StatusEnum.active)
+        e = PromotionFactory(status=StatusEnum.expired)
+        d = PromotionFactory(status=StatusEnum.deleted)
+        a.create(); e.create(); d.create()
+    
+        resp = self.client.get("/promotions?role=supplier")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        statuses = [p["status"] for p in data]
+        self.assertIn("active", statuses)
+        self.assertIn("expired", statuses)
+        self.assertNotIn("deleted", statuses)
+    # manager
+    def test_list_promotions_manager_all(self):
+        """Manager should see all promotions"""
+        a = PromotionFactory(status=StatusEnum.active)
+        e = PromotionFactory(status=StatusEnum.expired)
+        d = PromotionFactory(status=StatusEnum.deleted)
+        a.create(); e.create(); d.create()
+    
+        resp = self.client.get("/promotions?role=manager")
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(len(data), 3)
 
-    # Todo: Add your test cases here...
+    def test_list_promotions_invalid_role(self):
+        """It should return 400 for invalid role value"""
+        resp = self.client.get("/promotions?role=whoami")
+        self.assertEqual(resp.status_code, status.HTTP_400_BAD_REQUEST)
