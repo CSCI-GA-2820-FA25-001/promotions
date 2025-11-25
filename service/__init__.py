f26f43751cc7da7822f9b68153c31c8aa3e30313
Jackie Wen <129074656+ReidenGS@users.noreply.github.com>
2025-11-25 15:27:20 -0500

    Simplify static folder configuration in create_app
    Removed dynamic static folder path assignment.

--- service/__init__.py
+++ service/__init__.py
@@ -33,7 +33,6 @@ from service.common import log_handlers
 def create_app():
     """Initialize the core Flask application."""
     # Set static folder to root directory's static folder
-    static_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'static')
     app = Flask(
         __name__, static_folder="static", static_url_path="/static"
     )  # pylint: disable=redefined-outer-name
